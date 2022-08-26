8b2f02c7b1d2f09851795754ee055e74df7fad46
Bauke Molenaar <bauke.molenaar@gmail.com>
2022-08-26 05:54:51 +0200

    modified:   platformatari.py

--- platformatari.py
+++ platformatari.py
@@ -269,7 +269,7 @@ class PlatformLynx(PlatformCommon):
         self.run_process(emulator)
 
     def supported_platforms(self):
-        return ['atarijlynx', 'commodore64']
+        return ['atarijlynx']
 
 # Tries to identify files by any magic necessary
     def find_ext_files(self):
