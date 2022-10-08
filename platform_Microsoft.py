dc1cfce24b78239f8c1a668a66dd1647e2010360
Bauke Molenaar <bauke.molenaar@gmail.com>
2022-10-08 04:09:57 +0200

    modified:   platform_Microsoft.py

--- platform_Microsoft.py
+++ platform_Microsoft.py
@@ -186,7 +186,7 @@ class Platform_Windows(PlatformCommon):
     emulators = ['wine']
     cores = ['wine']
     fullscreens = ['false']
-    extensions = ['exe', 'bat', 'com']
+    extensions = ['exe']
     # wineprefix = self.showetdir + '/wineprefix'
 
     def run(self):
@@ -255,7 +255,7 @@ class Platform_Windows(PlatformCommon):
             size = os.path.getsize(file)
             if size > 0:
                 # Tries to exclude files that end with certain extensions/we dont need.. Grrgrrgll.
-                if file.endswith('.exe') or file.endswith('.bat') or file.endswith('.com'):
+                if not file.endswith('.json') and not file.endswith('.txt') and not file.endswith('.diz') and not file.endswith('.nfo') and not file.endswith('.png') and not file.endswith('.jpg') and not file.endswith('.JPG') and not file.endswith('.org') and not file.endswith('.org.txt'):
                     ext_files.append(file)
                     print("\tFound file: " + file)
         return ext_files
