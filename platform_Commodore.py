c4b70d8b25bc8ecd6f27c4d85f1a354e18c623eb
Bauke Molenaar <bauke.molenaar@gmail.com>
2022-10-18 04:09:29 +0200

    modified:   platform_3do.py 	modified:   platform_Commodore.py

--- platform_Commodore.py
+++ platform_Commodore.py
@@ -45,7 +45,6 @@ class Platform_Commodore64(PlatformCommon):
         recording = ['false']
         
         extensions = ['d64', 'd71', 'd80', 'd81', 'd82', 'g64', 'g41', 'x64', 't64', 'tap', 'prg', 'p00', 'crt', 'bin', 'zip', 'gz', 'd6z', 'd7z', 'd8z', 'g6z', 'g4z', 'x6z', 'cmd', 'm3u', 'vfl', 'vsf', 'nib', 'nbz', 'd2m', 'd4m']
-        
         floppys_ext = ['d64', 'd6z', 'd71', 'd7z', 'd80', 'd8z', 'd81', 'd82', 'd8z', 'g64', 'g6z', 'g41', 'g4z', 'x64', 'x6z', 'nib', 'nbz', 'd2m', 'd4m']
         tapes_ext = ['t64', 'tap', 'tcrt']
         roms_ext = ['prg', 'p00', 'crt', 'bin']
