684dc3a77be4a65a52dc74014df9dafad0548cea
Bauke Molenaar <bauke.molenaar@gmail.com>
2022-10-19 20:08:49 +0200

    modified:   showet.py

--- showet.py
+++ showet.py
@@ -194,7 +194,7 @@ if not os.path.exists(showetdir):
 else:
     print("Info: Folder %s exists." % showetdir)
 
-print("Check: iF Folder %s exists." % RetroPieEmuDir)
+print("Check: if Folder %s exists." % RetroPieEmuDir)
 if not os.path.exists(RetroPieEmuDir):
     print("Warning: %s does not exist" % RetroPieEmuDir)
     #exit(-1)
