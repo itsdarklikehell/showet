e44ce0293c6a36bb1db3f8f6567559eee8819983
Bauke Molenaar <bauke.molenaar@gmail.com>
2022-10-21 02:58:15 +0200

    modified:   platformcommon.py

--- platformcommon.py
+++ platformcommon.py
@@ -88,7 +88,7 @@ class PlatformCommon:
         emulator = inquirer.prompt(prompt).get('emulators').strip().lower()
         if debugging != False:
             print('You chose the selected emulator: ' + str(emulator))
-        return str(emulator)
+        return emulator
     
     def multicore(self,cores):
         #core = []
@@ -100,4 +100,4 @@ class PlatformCommon:
         core = inquirer.prompt(prompt).get('cores').strip().lower()
         if debugging != False:
             print('You chose the selected core: ' + str(core))
-        return str(core)
+        return core
